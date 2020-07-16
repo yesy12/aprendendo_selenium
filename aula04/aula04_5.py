ff3c283cccd1c70400679aac27f789900e765047
Alison <alisonvieira.av29@gmail.com>
2020-07-16 17:51:50 -0300

    Aula04

--- aula04/aula04_5.py
+++ aula04/aula04_5.py
@@ -1,10 +1,12 @@
 """
 1. Pegar todos os links de aulas - ok
-2. Navegar até o exercicio 3
+2. Navegar até o exercicio 3 - ok
 """
 
 from selenium.webdriver import Firefox
 from time import sleep
+from pprint import pprint
+
 driver = Firefox()
 
 url = "https://selenium.dunossauro.live/aula_04.html"
@@ -12,13 +14,21 @@ url = "https://selenium.dunossauro.live/aula_04.html"
 driver.get(url)
 
 sleep(4)
-as_ = driver.find_elements_by_xpath("//aside[@id='aside']/ul/li/a")
-hrefs = []
+aulas_as = driver.find_elements_by_xpath("//aside[@id='aside']/ul/li/a")
+conteudo = []
+aula = 3
 
-for a in as_:
-    href = a.get_attribute("href")
-    hrefs.append(href)
+for aula_a in aulas_as:
+    href = aula_a.get_attribute("href")
+    json = {
+       f"aula {aula}" : href
+    }
+    conteudo.append(json)
+    aula += 1
     
+pprint(conteudo)
+
+
 exe03 = driver.find_elements_by_xpath("//main[@id='main']/ul/li")[2]
 exe03_a = exe03.find_element_by_tag_name("a")
 exe03_link = exe03_a.get_attribute("href")
